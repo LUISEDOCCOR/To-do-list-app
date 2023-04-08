13837dc03848cb8b6cf8dc0dca78fb11f2d41da4
LuisEdOcCor <luiseduardoocegueda@gmail.com>
2023-04-07 18:11:02 -0600

    To do list app

--- index.php
+++ index.php
@@ -4,44 +4,30 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <script defer src="./js/index.js"></script>
-    <!-- Boostrap -->
+    <link rel="stylesheet" href="./css/main.css">    
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootswatch/5.2.3/darkly/bootstrap.min.css" integrity="sha512-YRcmztDXzJQCCBk2YUiEAY+r74gu/c9UULMPTeLsAp/Tw5eXiGkYMPC4tc4Kp1jx/V9xjEOCVpBe4r6Lx6n5dA==" crossorigin="anonymous" referrerpolicy="no-referrer" />    <title>To do list</title>
     <script defer src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="https://kit.fontawesome.com/91c956bb82.js" crossorigin="anonymous"></script>
+    <title>To do list</title>
 </head>
 <body>
     <header>
-        <nav class="navbar navbar-expand-lg navbar-dark bg-body-tertiary">
-            <div class="container-fluid">
-                <a class="navbar-brand" href="#">To do list</a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-                </button>
-                <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-                <div class="navbar-nav">
-                    <a class="nav-link" href="register.php">Register</a>
-                    <a class="nav-link" href="login.php">login</a>
-                    
-                </div>
-                </div>
-            </div>
-            </nav>
+        <hgroup>
+            <h1 id="h1">Your best to do list</h1>
+            <h2>Programmed in php</h2>
+        </hgroup>
     </header>
-    <!-- Page content-->
     <main>
-    <main>
-      <div class="welcome d-flex align-items-center justify-content-center">
-        <div class="text-center mt-5">
-          <h1>Your notebook of things to do</h1>
-          <h2 class="mt-3">Created by Luis</h2>
-          <h3 class="mt-3">With php</h3>
-          <a class="btn btn-lg btn-dark mt-3" href="register.php">Get Started</a>
+        <div class="separador">
+            <div class="welcome d-flex align-items-center justify-content-center">
+                <div class="text-center mt-4">
+                  <h3 class="mt-3">Start using it now</h3>
+                  <a class="btn btn-lg btn-dark mt-3" href="register.php">Get Started</a>
+                  <a href="https://github.com/LUISEDOCCOR/To-do-list-app" type="button" class="btn btn-info mt-3"><i class="fa-brands fa-github"></i></a>
+  
+                </div>
+              </div>
         </div>
-      </div>
-
-
-    </main>
     </main>
 </body>
 </html>
